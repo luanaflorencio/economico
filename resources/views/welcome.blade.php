3b0fe5f0e5677e69557e1927bf63dbf7a1c2024c
luanaflorencio <lua.m.016@gmail.com>
2021-01-27 22:43:12 -0300

    #2 Ajustes finais

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
                   </div>
                   <div class="col-md-8">
                     <div class="card-body">
-                    <h1 class="card-text">O <strong> economico</strong> é feito para aqueles que não tem medo de desafios e desejam evoluir acada um ultrapassado, aprenda a controlar se dinheiro se divertindo com seus amigos!</h1> 
+                    <h1 class="card-text">O <strong> economico</strong> é feito para aqueles que não tem medo de desafios e desejam evoluir a cada um ultrapassado, aprenda a controlar seu dinheiro se divertindo com seus amigos!</h1> 
                     </div>
                   </div>
                 </div>
@@ -45,11 +45,11 @@
           
         <div class="relative flex items-top justify-center min-h-screen bg-white sm:items-center sm:pt-0">
             @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                <div class="hidden fixed bottom-0 right-0 px-5 py-4 sm:block">
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Já é cadastrado? Entre agora</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Cadastre-se agora</a>
