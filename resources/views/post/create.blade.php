b421aefdc0c8c6314ae17acd47c3b05244cd2f99
luanaflorencio <lua.m.016@gmail.com>
2021-02-22 16:26:31 -0300

    #9 Ajustando front de create post

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,17 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    @include('includes.head')
-</head>
-<body>
-    @include('includes.header')
-
-    <form action="{{route('posts.store')}}" method="POST">
-    @csrf
-    <textarea name="challenge" id="" cols="30" rows="10" placeholder="Qual desafio deseja propor?"></textarea>
-    <input type="submit">
-    </form>
-
-    @include('includes.scripts')
-</body>
-</html>
+<x-guest-layout>
+    <x-auth-card>
+    <x-slot name="logo">
+    </x-slot>
+        
+    
+        <form action="{{route('posts.store')}}" method="POST" >
+        @csrf
+        
+            <textarea name="challenge" id="" cols="30" rows="10" placeholder="Qual desafio deseja propor?"></textarea>
+            <div class="grid grid-cols-4 position-absolute cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11.5 9H13v6h-1.5V9zM9 9H6c-.6 0-1 .5-1 1v4c0 .5.4 1 1 1h3c.6 0 1-.5 1-1v-2H8.5v1.5h-2v-3H10V10c0-.5-.4-1-1-1zm10 1.5V9h-4.5v6H16v-2h2v-1.5h-2v-1h3z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M18 13v7H4V6h5.02c.05-.71.22-1.38.48-2H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-5l-2-2zm-1.5 5h-11l2.75-3.53 1.96 2.36 2.75-3.54L16.5 18zm2.8-9.11c.44-.7.7-1.51.7-2.39C20 4.01 17.99 2 15.5 2S11 4.01 11 6.5s2.01 4.5 4.49 4.5c.88 0 1.7-.26 2.39-.7L21 13.42 22.42 12 19.3 8.89zM15.5 9C14.12 9 13 7.88 13 6.5S14.12 4 15.5 4 18 5.12 18 6.5 16.88 9 15.5 9z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M5 17v2h14v-2H5zm4.5-4.2h5l.9 2.2h2.1L12.75 4h-1.5L6.5 15h2.1l.9-2.2zM12 5.98L13.87 11h-3.74L12 5.98z"/></svg>
+        </div>
+        <x-button class="ml-52 bg-blue-500 hover:bg-blue-400">
+            {{ __('Enviar') }}
+        </x-button>
+     
+        
+           </form>
+    
+       
+    
+    </x-auth-card>
+    </x-guest-layout>
