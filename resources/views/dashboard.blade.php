d37c7b82e64589d54aa476503bc39c5a6777eff5
luanaflorencio <lua.m.016@gmail.com>
2021-03-09 00:17:30 -0300

    #16 Search box para busca dos usuários com datalist

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,7 @@
 
     <div class="">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @include('includes.search')
             <h3>Veja os desafios de seus amigos:</h3>
             <div class="mr-2">
                 @include('post')
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,7 +3,7 @@
 @include('includes.scripts')
    <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+        <div class="flex justify-between h-8">
            
             
             <div class="flex">
