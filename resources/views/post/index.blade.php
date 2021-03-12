0bc50b1168dd99b0128447f0eba5db7b5ee4f213
luanaflorencio <lua.m.016@gmail.com>
2021-03-11 22:46:56 -0300

    #16 Retirando search box da view index

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -4,7 +4,6 @@
 
     <div class="">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            @include('includes.search')
             <h3>Os desafios criados por você:</h3>
             <div class="mr-2">
                 @include('post')
